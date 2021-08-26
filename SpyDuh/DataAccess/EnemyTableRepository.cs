751f37370b5f3d7fec332e2875037c9415e1b25e
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-08-26 12:33:24 -0500

    Added functionality for adding enemies

---
+++ SpyDuh/DataAccess/EnemyTableRepository.cs
@@ -0,0 +1,40 @@
+﻿using SpyDuh.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyDuh.DataAccess
+{
+    public class EnemyTableRepository
+    {
+        static List<EnemyRelationshipTable> _enemyTable = new List<EnemyRelationshipTable>();
+
+        internal void Add(EnemyRelationshipTable relationshipTable)
+        {
+            _enemyTable.Add(relationshipTable);
+        }
+        internal List<EnemyRelationshipTable> Remove(Guid userId, Guid enemyId)
+        {
+            var enemyToRemove = _enemyTable.FirstOrDefault(table => table.UserId == userId && table.EnemyId == enemyId);
+            _enemyTable.Remove(enemyToRemove);
+            return _enemyTable;
+        }
+        internal IEnumerable<Guid> GetEnemies(Guid userId)
+        {
+            var tables = _enemyTable.Where(tables => tables.UserId == userId).ToList();
+            List<Guid> enemyList = new List<Guid>();
+            tables.ForEach(id => enemyList.Add(id.EnemyId));
+            return enemyList;
+        }
+        internal bool CheckUniqueEnemyTable(Guid userId, Guid enemyId)
+        {
+            var uniqueId = _enemyTable.FirstOrDefault(table => table.EnemyId == enemyId && table.UserId == userId);
+            if (uniqueId != null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SpyDuh/Models/EnemyRelationshipTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpyDuh.Models
+{
+    public class EnemyRelationshipTable
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid EnemyId { get; set; }
+    }
+}
